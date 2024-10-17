80b1eebb3286a788f259225ce33f8dbc3002afac
max2002 <chiayang.lu@gmail.com>
2024-10-17 16:12:19 +0800

    first commit

--- userdata_1.sh
+++ userdata_1.sh
@@ -5,4 +5,4 @@ sudo yum install nginx -y
 sudo systemctl start nginx
 sudo systemctl enable nginx
 sudo systemctl reload nginx
-echo "<h1>Hello, Welcome to on EC2-1!</h1>" | sudo tee /usr/share/nginx/html/index.html
+echo "<h1>Hello, Welcome to EC2-1!</h1>" | sudo tee /usr/share/nginx/html/index.html
